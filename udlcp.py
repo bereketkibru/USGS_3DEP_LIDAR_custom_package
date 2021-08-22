929a4fe6a7245cd43160e0fbddb5504d6c43c9fd
Bereket Kibru <bekakibru2@gmail.com>
2021-08-22 13:41:25 -0700

    add commnet to the package

--- udlcp.py
+++ udlcp.py
@@ -9,25 +9,44 @@ import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits import mplot3d
 
+"""
+Created on Aug 2021
+@author: Bereket Kibru
+"""
+
 
 logger = get_logger('FetchData')
 
 class FetchData:
+    """This is a class for fetching and manipulating point cloud data. The package will accept boundary polygons in shapely.geometry.Polygon,  and a coordinate reference system (CRS) and return  a geopandas grid point file with elevations encoded in the requested CRS.
+    """
+
 
     def __init__(self, public_data_url = "https://s3-us-west-2.amazonaws.com/usgs-lidar-public/", json_path="./get_data") -> None:
+        """This method is used to instantiate the class. It takes a  public data url for the point cloud data ,a json path to the pipline created for pdal and  a CRS EPSG value (i.e refer to https://epsg.io/) to use..
+        Args:
+            public_data_url (string): a url for the public point data storage
+            json_path (string): a file path for json pipline stored in assets folder
+            epsg (int): an integer EPSG value of coordinate reference system
+        """
+ 
         try:
             self.logger = get_logger(__name__)
             self.file_handler = FileHandler()
             self.pipeline_json = self.file_handler.read_json(json_path)
             self.public_data_url = public_data_url
             self.input_epsg = 3857
-            
-
             logger.info('Successfully Instantiated FetchData Class Object')
         except Exception as e:
             logger.exception('Failed to Instantiate FetchData Class Object')
             sys.exit(1)
     def get_polygon_boundaries(self, polygon: Polygon):
+        """This method accepts a Boundaries object which defines xmin, ymin, xmax, ymax.
+        Args:
+            bounds (Boundaries): a Boundaries object which defines a bound in form of xmin, ymin, xmax, ymax
+        Returns:
+            boundaries and polygon input
+        """
         polygon_df = gpd.GeoDataFrame([polygon], columns=['geometry'])
 
         polygon_df.set_crs(epsg=self.output_epsg, inplace=True)
@@ -47,6 +66,15 @@ class FetchData:
         return f"({[minx, maxx]},{[miny,maxy]})", polygon_input
 
     def get_pipeline(self, region: str, polygon: Polygon, output_filename: str = "farm"):
+        """This method accepts a  Region , a Boundaries object which defines xmin, ymin, xmax, ymax , output file name and returns a json file which fill the necessary fileds for pdal pipleine.
+        Args:
+            bounds (Boundaries): a Boundaries object which defines a bound in form of xmin, ymin, xmax, ymax
+            region (string) : Valid region name You can get a the lists in the assets/metadata.csv
+        Returns:
+            pipeline
+        """
+
+
         boundaries, polygon_input = self.get_polygon_boundaries(polygon)
 
         full_dataset_path = f"{self.public_data_url}{region}/ept.json"
@@ -63,6 +91,14 @@ class FetchData:
         return pipeline
 
     def run_pipeline(self, polygon: Polygon, epsg, region: str = "IA_FullState"):
+
+        """This method accepts a  Region , a Boundaries object which defines xmin, ymin, xmax, ymax , output file name and returns a json file which fill the necessary fileds for pdal pipleine.
+        Args:
+            bounds (Boundaries): a Boundaries object which defines a bound in form of xmin, ymin, xmax, ymax
+            region (string) : Valid region name You can get a the lists in the assets/metadata.csv
+        Returns:
+             excuted pipeline
+        """
         self.output_epsg = epsg
         pipeline = self.get_pipeline(region, polygon)
 
@@ -75,6 +111,12 @@ class FetchData:
             print(e)
 
     def make_geo_df(self, arr):
+        
+        """This method saves a geopandas dataframe containing elevation points as a csv file and geojson file.
+        Args:
+            arr (pipeline.arrays[0]):
+            
+        """
         geometry_points = [Point(x, y) for x, y in zip(arr["X"], arr["Y"])]
         elevetions = arr["Z"]
         df = gpd.GeoDataFrame(columns=["elevation", "geometry"])
@@ -86,6 +128,13 @@ class FetchData:
         df.to_file("./data/geo/farm.geojson", driver='GeoJSON')
         return df
     def get_data(self, polygon: Polygon, epsg):
+        """This method accepts a  Region , a Boundaries object which defines xmin, ymin, xmax, ymax , output file name and returns function call.
+        Args:
+            bounds (Boundaries): a Boundaries object which defines a bound in form of xmin, ymin, xmax, ymax
+            epsg (int): an integer EPSG value of coordinate reference system
+        Returns:
+             function self.make_geo_df(arr)
+        """
         pipeline = self.run_pipeline(polygon, epsg)
         arr = pipeline.arrays[0]
         return self.make_geo_df(arr)
@@ -94,7 +143,15 @@ logger = get_logger('Visulaize')
 logger.info("Starts Visualize script")
 
 class Visulaize:
+    """This is a class for visualizing point cloud data. The package will accept a geojsaon file.
+     The package will  provide an option to graphically display the returned elevation files as a 3D plot.
+    """
     def __init__(self,geoData="farm" ) -> None:
+        """This method is used to instantiate the class. It takes a geojson file name and returns a 3 plot of the file and save the image in image folder.
+        Args:
+            
+            geoData (string): a file name for the geojson file stored in data/geo
+        """
         try:
             self.logger = get_logger(__name__)
             self.geoData = geoData
@@ -105,6 +162,9 @@ class Visulaize:
             sys.exit(1)
     
     def plot_3d_map(self):    
+        """Plots a 3D terrain scatter plot for the cloud datapoints of geopandas dataframe using matplotlib
+            
+        """
         gdf = self.file_handler.read_gdf(self.geoData)
         gdf.crs = "epsg:4326"
         x = gdf.geometry.x
